f77e3c58a622bb753d5f8cf970eb24c545bfd5de
sushil gyawali <63179543+Sushil787@users.noreply.github.com>
2020-09-13 17:29:15 +0545

    Create program.cpp

---
+++ program.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+ namespace first{
+void display(){
+cout<<"hello sushil gyawali \n";
+
+
+
+}
+
+}
+void display(){
+
+
+cout<<"hello sushil Ai am different ";
+
+
+}
+int main()
+{
+ first::display();
+ display();
+ return 0;
+}
